eb452a19617773c06a8926f437a17441597810d5
Анна Андрус <anna.andrus17@gmail.com>
2025-04-08 01:28:36 +0500

    моковые данные + модель

--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
+import FilterView from '../view/filters-view';
 import Presenter from './presenter/main-presenter.js';
+import PointsModel from './model/main-model.js';
+import { getPoints, getDestinations, getOffersByType } from './mock/points-mock.js';
+import { render } from './render.js';
 
-const presenter = new Presenter();
 
-presenter.init();
+const filterContainer = document.querySelector('.trip-controls__filters');
+const tripContainer = document.querySelector('.trip-events');
+const newPresenter = new Presenter(tripContainer);
+
+const points = getPoints();
+const offersByType = getOffersByType();
+const destinations = getDestinations();
+const pointsModel = new PointsModel();
+
+render(new FilterView(), filterContainer);
+
+pointsModel.init(points, destinations, offersByType);
+newPresenter.init(pointsModel);
